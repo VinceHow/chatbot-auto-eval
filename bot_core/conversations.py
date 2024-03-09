46ff551d7e9f9066fc32ee2bd8c878198b957371
Vincent Hao <vincenthao@monzo.com>
2024-03-09 14:08:03 +0000

    Add 1 turn convo

---
+++ bot_core/conversations.py
@@ -0,0 +1,8 @@
+convo = {
+    'conversation_seed': {'job_to_be_done': 'Discover new and unique snacks', 'user_query': 'What kind of snacks can I expect in each box?'}, 
+    'interactions': [
+        {'interaction_turn': 1, 'user_query': 'What kind of snacks can I expect in each box?', 'bot_response': "Each Snack 52 box contains a curated selection of premium, artisanal snacks from around the world. You'll discover unique flavors, healthy options, and indulgent treats tailored to your preferences. Our snack experts carefully source the finest snacks to delight your taste buds every month.", 'knowledge_used': [], 'evaluation': {'faithfulness': 0.0, 'context_precision': 0.0, 'answer_relevancy': 0.0, 'context_recall': 0.0}}
+        ], 
+    'interaction_turns': 0,
+    'evaluation': None
+    }
